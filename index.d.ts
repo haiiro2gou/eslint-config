d0b4eed1283375d791fba609c05b8b230eb50a0f
はいいろ <mrashen.2gou@gmail.com>
2025-06-03 09:40:51 +0900

    :label: Added type declaration

---
+++ index.d.ts
@@ -0,0 +1,6 @@
+declare module "@haiiro2gou/eslint-config" {
+    export const essentials: import('eslint').Linter.Config[];
+    export const node: import('eslint').Linter.Config[];
+    export const react: import('eslint').Linter.Config[];
+    export const typescript: import('eslint').Linter.Config[];
+}
